3780e38256d2ae920e07eb686e2da07f7c62772f
Tom Gumbuo <gumbuogw3@gmail.com>
2025-09-21 21:24:08 -0400

    Add thirdweb/react type declaration

--- thirdweb.d.ts
+++ thirdweb.d.ts
@@ -2,3 +2,7 @@ declare module 'thirdweb' {
   export const createThirdwebClient: any;
   export const defineChain: any;
 }
+
+declare module 'thirdweb/react' {
+  export const ConnectButton: any;
+}
