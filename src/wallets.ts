577f88c6ffb8a848bdc644829526b20b1b9f0905
Tom Gumbuo <gumbuogw3@gmail.com>
2025-09-21 22:16:28 -0400

    Use 'new' for wallet constructors

--- src/wallets.ts
+++ src/wallets.ts
@@ -6,8 +6,8 @@ import {
 } from "@thirdweb-dev/wallets";
 
 export const wallets = [
-  MetaMaskWallet(),
-  CoinbaseWallet(),
-  WalletConnect(),
-  PhantomWallet(),
+  new MetaMaskWallet(),
+  new CoinbaseWallet(),
+  new WalletConnect(),
+  new PhantomWallet(),
 ];
