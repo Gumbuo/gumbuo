2819a315e0a9dcb6a9b8a64eceba72267a641214
Tom Gumbuo <gumbuogw3@gmail.com>
2025-09-23 19:59:52 -0400

    Rename config to next.config.js for Vercel compatibility

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
@@ -5,4 +6,4 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
